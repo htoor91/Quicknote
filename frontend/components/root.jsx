dd8b6e8990a67d766bf07e67c4589373cde54352
Hassan Toor <hassantoor8@gmail.com>
2016-12-06 17:41:34 -0500

    Add comment

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -4,7 +4,7 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import App from './app';
 import SessionFormContainer from './session_form/session_form_container';
 
-
+//some comment
 
 const Root = ({ store }) => {
 
