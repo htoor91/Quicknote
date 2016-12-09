b30da7f544fddee043f72434cf0fa72fadff790e
Hassan Toor <hassantoor8@gmail.com>
2016-12-08 19:42:32 -0500

    Add selectors

--- frontend/components/home/home_container.js
+++ frontend/components/home/home_container.js
@@ -1,18 +1,24 @@
 import { connect } from 'react-redux';
 import Home from './home';
-import { logout } from '../../actions/session_actions';
+import { fetchNote, fetchNotes, createNote, updateNote, deleteNote } from '../../actions/note_actions';
+import { allNotes } from '../../reducers/selectors';
 
 
 const mapStateToProps = ( state ) => {
 
   return ({
+    allNotes: allNotes(state),
   });
 };
 
 const mapDispatchToProps = (dispatch) => {
 
   return ({
-    logout: () => dispatch(logout())
+    fetchNote: (note) => dispatch(fetchNote(note)),
+    fetchNotes: () => dispatch(fetchNotes()),
+    createNote: (note) => dispatch(createNote(note)),
+    updateNote: (note) => dispatch(updateNote(note)),
+    deleteNote: (note) => dispatch(deleteNote(note))
   });
 };
 
---
+++ frontend/reducers/selectors.js
@@ -0,0 +1,3 @@
+export const allNotes = ({ notes }) => Object.keys(notes).map((id) => {
+  return notes[id];
+});
