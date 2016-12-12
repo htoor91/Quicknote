316c5c748d74f9c9e5739e3f1987f29379a6db5c
Hassan Toor <hassantoor8@gmail.com>
2016-12-12 15:58:37 -0500

    Remove unnecessary modal content style

--- frontend/components/modal_styles/session_modal_styles.js
+++ frontend/components/modal_styles/session_modal_styles.js
@@ -6,15 +6,5 @@ export const SessionModalStyle = {
     right: 0,
     bottom: 0,
     backgroundColor: 'rgba(150, 150, 150, 0.75)'
-  },
-  content: {
-    position: 'fixed',
-    width: '400px',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%,-50%)',
-    display: 'flex',
-    align_items: 'center',
-    flex_direction: 'column'
   }
 };
