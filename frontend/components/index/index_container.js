a5db27716f633724832e03cc8103ba9dccc32778
Hassan Toor <hassantoor8@gmail.com>
2016-12-13 09:18:10 -0500

    Add notebook and tag state to props

--- frontend/components/index/index_container.js
+++ frontend/components/index/index_container.js
@@ -1,10 +1,16 @@
 import { connect } from 'react-redux';
 import Index from './index';
+import { fetchNotes } from '../../actions/note_actions';
+import { fetchNotebooks } from '../../actions/notebook_actions';
 
 const mapStateToProps = state => {
   return ({
     currentUser: state.session.currentUser,
-    notes: state.notes.allNotes
+    notes: state.notes.allNotes,
+    notebooks: state.notebooks.allNotebooks,
+    currentNotebook: state.notebooks.currentNotebook,
+    taggedNoted: state.notes.taggedNotes,
+    currentTag: state.tags.currentTag
   });
 };
 
