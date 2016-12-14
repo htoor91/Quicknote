dbc5c6ab79d7977693301ff7e5783715f77289a5
Hassan Toor <hassantoor8@gmail.com>
2016-12-14 01:23:09 -0500

    Add dependent destroy to tagging

--- app/models/note.rb
+++ app/models/note.rb
@@ -19,7 +19,7 @@ class Note < ActiveRecord::Base
     foreign_key: :author_id
   )
   belongs_to :notebook
-  has_many :taggings
+  has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings, source: :tag
 
   validates :author, :notebook, presence: true
